3d76cb441fe49a9cf63dbe827b28de420034e1d6
Ben Mohammad <benmohammad593@gmail.com>
2020-06-18 12:39:13 +0100

    BaseDialog added...

--- app/src/main/java/com/benmohammad/newshakz/ui/main/BaseViewModel.kt
+++ app/src/main/java/com/benmohammad/newshakz/ui/main/BaseViewModel.kt
@@ -1,21 +1,56 @@
 package com.benmohammad.newshakz.ui.main
 
 import android.app.Application
+import androidx.databinding.Observable
+import androidx.databinding.PropertyChangeRegistry
 import androidx.lifecycle.AndroidViewModel
-import io.reactivex.Observable
 import io.reactivex.disposables.CompositeDisposable
-
-
 
 abstract class BaseViewModel(
     app: Application,
     protected val disposables: CompositeDisposable = CompositeDisposable()
-) : AndroidViewModel(app) {
+) : AndroidViewModel(app), Observable {
 
     override fun onCleared() {
         disposables.dispose()
         super.onCleared()
     }
 
+    @Transient
+    private var dataBindingCallbacks: PropertyChangeRegistry? = null
+    override fun addOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
+        synchronized(this) {
+            if(dataBindingCallbacks == null) {
+                dataBindingCallbacks = PropertyChangeRegistry()
+            }
+        }
+        dataBindingCallbacks!!.add(callback)
+    }
 
-}
+    override fun removeOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
+        synchronized(this) {
+            if(dataBindingCallbacks == null) {
+                return
+            }
+        }
+
+        dataBindingCallbacks!!.remove(callback)
+    }
+
+    fun notifyChange() {
+        synchronized(this) {
+            if(dataBindingCallbacks == null) {
+                return
+            }
+        }
+        dataBindingCallbacks!!.notifyCallbacks(this, 0, null)
+    }
+
+    fun notifyPropertyChanged(fieldId: Int) {
+        synchronized(this) {
+            if(dataBindingCallbacks == null) {
+                return
+            }
+        }
+        dataBindingCallbacks!!.notifyCallbacks(this, fieldId, null)
+    }}
